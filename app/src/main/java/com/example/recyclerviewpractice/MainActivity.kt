d8b2385bc0bdd5370cf95fe5694d312deb802c4d
kyupkyup <rudduqdl@naver.com>
2020-12-04 08:46:04 +0900

    Initial commit

---
+++ app/src/main/java/com/example/recyclerviewpractice/MainActivity.kt
@@ -0,0 +1,19 @@
+package com.example.recyclerviewpractice
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.RecyclerView
+import com.example.recyclerviewpractice.adapter.ItemAdapter
+import com.example.recyclerviewpractice.data.DataSource
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val myDataset = DataSource().loadAffirmations()
+        val recyclerView = findViewById<RecyclerView>(R.id.recycler_view)
+        recyclerView.adapter = ItemAdapter(this, myDataset)
+        recyclerView.setHasFixedSize(true)
+    }
+}
